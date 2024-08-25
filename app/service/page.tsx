1219496b64a061362ba9f352f53e2ebc4d19dea8
Tafon_Hubert <tafonsoftwarespecialist@gmail.com>
2024-08-25 14:04:31 +0100

    creating the service page

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
 import banner from '@/public/assets/dst-bg-img.jpg'
-import aboutImg4 from '@/public/assets/about_img.jpg'
-import service from '@/public/assets/service-img07.jpg'
-import serviceImg2 from '@/public/assets/sevice-img08.jpg'
+import serviceLaundry from '@/public/assets/laundry.png'
+import service from '@/public/assets/service-img07.png'
+import serviceImg2 from '@/public/assets/service-img08.png'
 import laundry from '@/public/assets/laundry.webp'
-import { GiHanger, GiWashingMachine } from 'react-icons/gi'
+import { GiHanger, GiWashingMachine, GiConverseShoe } from 'react-icons/gi'
+import { PiShirtFoldedFill } from "react-icons/pi";
 import Link from 'next/link'
-import { TbIroningSteam } from "react-icons/tb";
+import { TbIroningSteam, TbTruckDelivery } from "react-icons/tb";
 import { HiClipboardDocumentList } from 'react-icons/hi2'
+import { MdEco } from 'react-icons/md'
 
 const Service = () => {
   return (
@@ -37,8 +39,8 @@ const Service = () => {
                 <GiWashingMachine size={40} className='text-white '/>
               </div>
             </div>
-            <div className='pt-8'>
-              <h6>Laundry Services</h6>
+            <div className='pt-8 px-[11%]'>
+              <h6 className='py-4'>Laundry Services</h6>
               <span>Let us pick up your dirty laundry, sort it, pre-treat stains, wash, dry, fold and deliver back to you in one neat, easy package.</span>
               <div>
                 <Link href=''><button className='font-bold bg-[#18F0F0] p-4 text-white mt-4'>More Info</button></Link>
@@ -52,8 +54,8 @@ const Service = () => {
                 < GiHanger size={40} className='text-white '/>
               </div>
             </div>
-            <div className='pt-8'>
-              <h6>Dry cleaning Services</h6>
+            <div className='pt-8 px-[11%]'>
+              <h6 className='py-4'>Dry cleaning Services</h6>
               <span>UB students and local residents love on our reliable dry cleaning services for the fast, accurate, top quality results.</span>
               <div>
                 <Link href=''><button className='font-bold bg-[#18F0F0] p-4 text-white mt-4'>More Info</button></Link>
@@ -62,13 +64,13 @@ const Service = () => {
           </div>
           <div className="flex flex-col items-center">
             <div className='relative'>
-              <Image src={aboutImg4} alt='About-us-img-4' />
+              <Image src={serviceLaundry} alt='About-us-img-4' />
               <div className=' p-4 absolute bottom-[-7%] left-[6%]  rounded-full bg-[#18F0F0] self-start shadow-gray-300 shadow-lg'>
                 <TbIroningSteam size={40} className='text-white '/>
               </div>
             </div>
-            <div className='pt-8'>
-              <h6>Steam Iron</h6>
+            <div className='pt-8 px-[11%]'>
+              <h6 className='py-4'>Steam Iron</h6>
               <span>These services are accomplished under the guidance of adroit personnel who have affluent industry proficiency.</span>
               <div>
                 <Link href=''><button className='font-bold bg-[#18F0F0] p-4 text-white mt-4'>More Info</button></Link>
@@ -95,49 +97,51 @@ const Service = () => {
           </div>
         </div>
         <div className="container m-auto">
-          <div className="grid grid-cols-2 gap-8">
-              <div className="flex space-x-4 p-4">
+        <div className='flex flex-col items-center '>
+            <h5 className=" font-bold py-4  text-[#18F0F0]">[ Why you'll love us ]</h5>
+            <div className='text-center md:w-[50%] py-4'>
+              <h2>Our Features</h2>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-8 p-8">
+              <div className="flex space-x-4 p-8">
                 <div className='p-4 rounded-full bg-slate-100  self-start shadow-gray-300 shadow-lg'>
-                <HiClipboardDocumentList size={40} className='text-[#18F0F0]'/>
+                <MdEco size={40} className='text-[#18F0F0]'/>
                 </div>
                 <div>
-                  <h5>Instant Order Update</h5>
+                  <h5 className='py-2'>Eco-Friendly Dry Cleaning</h5>
                   <span>
-                  Regular updates of your order. to help you keep a track of your laundry and plan accordingly.
-                  </span>
+                  Our commitment to eco-friendly practices extends beyond our use of 100% toxin-free cleaning techniques. Our company is also incredibly proud to be a carbon-neutral business.                  </span>
+                </div>
+              </div>
+              <div className="flex space-x-4 p-8">
+                <div className='p-4 rounded-full bg-slate-100  self-start shadow-gray-300 shadow-lg'>
+                <PiShirtFoldedFill size={40} className='text-[#18F0F0]'/>
+                </div>
+                <div>
+                  <h5 className='py-2'>Wash & Fold</h5>
+                  <span>
+                  We are excited to offer quality shoe repair & bag services alongside our second to none dry cleaning and wash & fold service. Our staff use the highest quality products.                  </span>
                 </div>
               </div>
               <div className="flex space-x-4 p-4">
                 <div className='p-4 rounded-full bg-slate-100  self-start shadow-gray-300 shadow-lg'>
-                <HiClipboardDocumentList size={40} className='text-[#18F0F0]'/>
+                <GiConverseShoe size={40} className='text-[#18F0F0]'/>
                 </div>
                 <div>
-                  <h5>Instant Order Update</h5>
+                  <h5 className='py-2'>Bag & Shoe Shines</h5>
                   <span>
-                  Regular updates of your order. to help you keep a track of your laundry and plan accordingly.
-                  </span>
+                  Laundry services are considerably the most convenient services that people look for. Laundry is one of the most difficult chores that we have to deal with on a regular basis.                  </span>
                 </div>
               </div>
               <div className="flex space-x-4 p-4">
                 <div className='p-4 rounded-full bg-slate-100  self-start shadow-gray-300 shadow-lg'>
-                <HiClipboardDocumentList size={40} className='text-[#18F0F0]'/>
+                <TbTruckDelivery size={40} className='text-[#18F0F0]'/>
                 </div>
                 <div>
-                  <h5>Instant Order Update</h5>
+                  <h5 className='py-2'>Package Delivery</h5>
                   <span>
-                  Regular updates of your order. to help you keep a track of your laundry and plan accordingly.
-                  </span>
-                </div>
-              </div>
-              <div className="flex space-x-4 p-4">
-                <div className='p-4 rounded-full bg-slate-100  self-start shadow-gray-300 shadow-lg'>
-                <HiClipboardDocumentList size={40} className='text-[#18F0F0]'/>
-                </div>
-                <div>
-                  <h5>Instant Order Update</h5>
-                  <span>
-                  Regular updates of your order. to help you keep a track of your laundry and plan accordingly.
-                  </span>
+                  We offer one-day day laundry service to customers in our service areas, combining the excellence of dry cleaning with the ultimate convenience in laundry service and laundry delivery.                  </span>
                 </div>
               </div>
           </div>
